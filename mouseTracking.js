196eaec4fdf28b4b1e8cf827943469cfefd13e4c
tblume1992 <t-blume@hotmail.com>
2018-07-03 09:56:05 -0400

    Create mouseTracking.js

---
+++ mouseTracking.js
@@ -0,0 +1,19 @@
+// event handler function
+function handler(e) {
+    e = e || window.event;
+
+    var pageX = e.pageX;
+    var pageY = e.pageY;
+
+    // IE 8
+    if (pageX === undefined) {
+        pageX = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
+        pageY = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
+    }
+
+    console.log(pageX, pageY);
+}
+
+// attach handler to the click event of the document
+if (document.attachEvent) document.attachEvent('onclick', handler);
+else document.addEventListener('click', handler);
